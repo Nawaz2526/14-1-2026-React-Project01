5e275ea9082d19da8eda88a007b67ee5e3c244e1
Nawaz2526 <nawazsidd245@gmail.com>
2026-01-14 01:35:59 -0700

    small change

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,43 +1,98 @@
 import React from 'react'
-import "./App.css"
 import { CiBookmark } from "react-icons/ci";
-const Card = () => {
+import "./App.css"
+
+const Card = ({ elem }) => {
   return (
-    <div>
-        <div className="perent">
-          <div className="card">
-            <div className='top'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtBf9Np5QEUSwMLpiqsr-hpKdnp32D2nlO1w&s" alt="" />
-            <button>Save<CiBookmark /></button>
-            </div>
-    
-    
-            <div className="center">
-            <div className='center1'>
-              <h3>Amazon <span>5 day ago</span></h3> 
-             <h2>Senior UI\UX Designer</h2>
-            </div>
-           
-            <div className="center3">
-             <h4>Part time</h4>
-             <h4>Senior level</h4>
-            </div>
-            </div>
-    
-            <div className="botum">
-              <div className="botum1">
-                <h3>
-                  $120\hr
-                </h3>
-                <p>Mombai India</p>
-              </div>
-              <div className="botum2">
-                <button>Apply Now</button>
-              </div>
-            </div>
+   
+      <div className="card">
+
+        <div className="top">
+          <img src={elem.companyLogo} alt="" />
+          <button>Save <CiBookmark /></button>
+        </div>
+
+        <div className="center">
+          <div className="center1">
+            <h3>
+              {elem.companyName} <span>{elem.time}</span>
+            </h3>
+            <h2>{elem.jobName}</h2>
           </div>
-          </div></div>
+
+          <div className="center3">
+            {elem.jobType.map((type, index) => (
+              <h4 key={index}>{type}</h4>
+            ))}
+          </div>
+        </div>
+
+        <div className="botum">
+          <div className="botum1">
+            <h3>{elem.pay}</h3>
+            <p>{elem.location}</p>
+          </div>
+
+          <div className="botum2">
+            <button>Apply Now</button>
+          </div>
+        </div>
+
+      </div>
+    
   )
 }
 
-export default Card
+export default Card;
+
+
+
+
+// import React from 'react'
+// import "./App.css"
+// import { CiBookmark } from "react-icons/ci";
+
+// const Card = ({ elem }) => {
+//   return (
+//     <div>
+//       <div className="perent">
+//         <div className="card">
+
+//           <div className='top'>
+//             <img src={elem.companyLogo} alt="" />
+//             <button>Save<CiBookmark /></button>
+//           </div>
+
+//           <div className="center">
+//             <div className='center1'>
+//               <h3>
+//                 {elem.companyName} <span>{elem.time}</span>
+//               </h3>
+//               <h2>{elem.jobName}</h2>
+//             </div>
+
+//             <div className="center3">
+//               {elem.jobType.map((type, index) => (
+//                 <h4 key={index}>{type}</h4>
+//               ))}
+//             </div>
+//           </div>
+
+//           <div className="botum">
+//             <div className="botum1">
+//               <h3>{elem.pay}</h3>
+//               <p>{elem.location}</p>
+//             </div>
+
+//             <div className="botum2">
+//               <button>Apply Now</button>
+//             </div>
+//           </div>
+
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Card;
